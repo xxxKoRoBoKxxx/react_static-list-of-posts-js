835f09bac1e7a2e09c1c1d1a1cfa38d20779a0d5
Andrii Filipenko <andreyfil1337@gmail.com>
2025-04-16 19:30:52 +0300

    work in progress

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,17 @@
-export const PostList = () => <>Put the list here</>;
+import './PostList.scss';
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts, comments }) => (
+  <div className="PostList">
+    {posts.forEach(post => {
+      comments.forEach(comment => console.log(comment.postId === post.id));
+
+      // return (
+      //   <PostInfo
+      //     post={post}
+      //     comments={comments.filter(comment => post === comment.post)}
+      //   />
+      // );
+    })}
+  </div>
+);
